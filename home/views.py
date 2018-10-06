969f7f345b5833b6b2bf76752088f963a20d3102
Ubuntu <ubuntu@ip-172-31-40-14.us-east-2.compute.internal>
2018-10-06 04:08:02 +0000

    testing changes with CodeDeploy

--- home/views.py
+++ home/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! You’re at the home index")
+    return HttpResponse("Hello, world! You’re at the home index. I'm testing CodeDeploy changes and I hope they work.")
 
